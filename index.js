2e1073038f7d493a36743c7631defd4f05f37fdd
Daniel Cheong <dcheo001@gmail.com>
2021-11-04 17:48:37 -0400

    commit tree af23d348129b554bf222f6cf96d7a92d0af13259

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (driverArray, name){
+    const newDrivers = driverArray.filter(person => person.toLowerCase() === name.toLowerCase());
+    return newDrivers;
+}
+// function fuzzyMatch (driverArray, driverName){
+//     return driverArray.filter(element => element.slice(0,1) === driverName.slice(0,1))
+//     }
+    const fuzzyMatch = (array, name) => {
+        const newArray = array.filter(name2 => name2.substring(0,2) === name )
+        return newArray
+    }
+    
+function matchName(object, hometown) {
+    return object.filter(driver => driver.name === hometown);
+}
